8670c57d395122f536a90c2183ad91ea9d415fd4
Joemon <joemon@joemon>
2021-05-22 12:14:37 +0530

    Commit  1

---
+++ src/main/java/com/joe/SpringBootWithDockerJenkinsKubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.joe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithDockerJenkinsKubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithDockerJenkinsKubernetesApplication.class, args);
+	}
+
+}
